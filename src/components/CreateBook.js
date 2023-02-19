2314f83f178bdd643c01268f785cefb8b5a029c1
tektechnologies <info@tektechnologies.com>
2023-02-18 20:56:45 -0600

    update and format create book component

--- src/components/CreateBook.js
+++ src/components/CreateBook.js
@@ -1,48 +1,39 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import { Container } from 'react-bootstrap';
-import BookFormModal from '../components/BookFormModel';
-// import axios from "axios";
+import React from "react";
+import Button from "react-bootstrap/Button";
+import { Container } from "react-bootstrap";
+import BookFormModal from "../components/BookFormModel";
 
 class CreateBook extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showModal: false
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+    };
+  }
 
-    handleShowModal = () => {
-        this.setState({ showModal: true });
-    }
+  handleShowModal = () => {this.setState({ showModal: true });};
+  handleHideModal = () => {this.setState({ showModal: false });};
 
-    handleHideModal = () => {
-        this.setState({ showModal: false });
-    }
+  handleSubmit = (book) => {
+    this.props.onSubmit(book);
+  };
 
-    handleSubmit = (book) => {
-        // Call the parent component's onSubmit function with the book object
-        this.props.onSubmit(book);
-    }
+  render() {
+    return (
+      <Container className="mt-5">
+        <Button
+          variant="outline-secondary"
+          onClick={() => this.handleShowModal()}
+        >Add Book</Button>
 
-    render() {
-        return (
-            <Container className="mt-5">
-                <Button variant="outline-secondary" onClick={
-                    () => this.handleShowModal()}>Add Book</Button>
-               
-             
-              
-               
-               
-                <BookFormModal
-                    show={this.state.showModal}
-                    handleBookSubmit={this.props.handleBookSubmit}
-                    onHide={this.handleHideModal}
-                />
-            </Container>
-        );
-    }
+        <BookFormModal
+          show={this.state.showModal}
+          handleBookSubmit={this.props.handleBookSubmit}
+          onHide={this.handleHideModal}
+        />
+      </Container>
+    );
+  }
 }
 
 export default CreateBook;
