40f7a9e98a47f370395fc60fb606bce588eef42c
tektechnologies <info@tektechnologies.com>
2023-02-18 21:14:37 -0600

    add margin to description to push button down

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./components/Books.js";
 
 import Button from "react-bootstrap/Button";
 import CreateBook from "./components/CreateBook.js";
-import About from "./components/About/About.js";
+// import About from "./components/About/About.js";
 import Header from "./components/Header/Header.js";
 
 let SERVER = process.env.REACT_APP_SERVER;
@@ -22,6 +22,7 @@ class App extends React.Component {
   getBooks = async () => {
     try {
       let results = await axios.get(`${SERVER}/books`);
+      console.log(results);
       this.setState({
         books: results.data,
       });
@@ -30,58 +31,57 @@ class App extends React.Component {
     }
   };
 
-  handleBookSubmit = async (event) => {
-    event.preventDefault();
+  // handleBookSubmit = async (event) => {
+  //   event.preventDefault();
+  //   let newBook = {
+  //     title: event.target.title.value,
+  //     description: event.target.description.value,
+  //     status: event.target.status.checked,
+  //   };
+  // this.postBook(newBook);
+  // };
 
-    let newBook = {
-      title: event.target.title.value,
-      description: event.target.description.value,
-      status: event.target.status.checked,
-    };
-    this.postBook(newBook);
-  };
+  // postBook = async (newBookObject) => {
+  //   try {
+  //     let url = `${SERVER}/books`;
+  //     let createdBook = await axios.post(url, newBookObject);
+  //     this.setState({
+  //       books: [...this.state.books, createdBook.data],
+  //     });
+  //   } catch (error) {
+  //     console.log("We have an error: ", error.response.data);
+  //   }
+  // };
 
-  postBook = async (newBookObject) => {
-    try {
-      let url = `${SERVER}/books`;
-      let createdBook = await axios.post(url, newBookObject);
-      this.setState({
-        books: [...this.state.books, createdBook.data],
-      });
-    } catch (error) {
-      console.log("We have an error: ", error.response.data);
-    }
-  };
-
-  deleteBook = async (bookToDelete) => {
-    try {
-      let url = `${SERVER}/books/${bookToDelete._id}`;
-      await axios.delete(url);
-      let updatedBooks = this.state.books.filter(
-        (book) => book._id !== bookToDelete._id
-      );
-      this.setState({
-        books: updatedBooks,
-      });
-    } catch (error) {
-      console.log("We have an error: ", error.response.data);
-    }
-  };
+  // deleteBook = async (bookToDelete) => {
+  //   try {
+  //     let url = `${SERVER}/books/${bookToDelete._id}`;
+  //     await axios.delete(url);
+  //     let updatedBooks = this.state.books.filter(
+  //       (book) => book._id !== bookToDelete._id
+  //     );
+  //     this.setState({
+  //       books: updatedBooks,
+  //     });
+  //   } catch (error) {
+  //     console.log("We have an error: ", error.response.data);
+  //   }
+  // };
 
   componentDidMount() {
     this.getBooks();
   }
 
   render() {
+    console.log(this.state.books)
     let books = this.state.books.map((book) => (
       <Carousel.Item key={book._id}>
-        {/* <Carousel.Caption> */}
-        <h2>{book.title}</h2>
-        <p>{book.description}</p>
+            <h2>{book.title}</h2>
+            <p className="book-desc">{book.description}</p>
+     
         <Button variant="danger" onClick={() => this.deleteBook(book)}>
           Delete Book
         </Button>
-        {/* </Carousel.Caption> */}
       </Carousel.Item>
     ));
     return (
